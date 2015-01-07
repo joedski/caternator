78b41f3432ef44f8b7e7b36aa24514e18caf10da
Joseph Sikorski <joedski@gmail.com>
2015-01-06 23:57:17 -0500

    Added some code.  Have to wrap all Lines because I want to store data about line number, etc.

--- src/caternator/parser.js
+++ src/caternator/parser.js
@@ -2,35 +2,71 @@
 
 var tokenizer = require( 'caternator/tokenizer' );
 
+// String -> Thing
 function parse( input ) {
 	// - tokenize
-	// - statementize -> output statements, variable definitions, function definitions
-	// - statements' contents:
-	//     - unflatten
-	//     - interpret special forms
-	//     - create output objects (AlternationSets, etc...)
+	var normalizedInput = normalize( input );
+	var lineList = splitLines( normalizedInput );
+	var tokensLineList = lineList.map( tokenizer.tokenize );
+	var identifiedTokensList = tokensLineList.map( tokenizer.identifyTokens );
+	var nestedTokensLineList = tokensLineList.map( nestTokens );
 
-	var tokens = tokenizer.tokenize( input );
-	var statements = parseStatements( tokens );
-
-	return statements;
+	return {};
 }
 
-function parseStatements( tokens ) {
-	return tokens.map( function parseSingleStatement( lineTokens, i ) {
+// normalize line-endings
+// space out certain characters: "(", ")", "="
+function normalize( inputString :String ) :String {
+	return inputString
+		.replace( /\r\n?/g, '\n' ) // normalize line endings.  (theoretically shouldn't matter?)
+		.replace( /[\(\)=]/g, ' $& ' ) // space out tokens.
+		;
+}
+
+function splitLines( normalizedInput :String ) :Array {
+	var lines = normalizedInput.split( '\n' );
+	return lines.map( function addMeta( lineString, index ) {
+		return {
+			string: lineString,
+			meta: {
+				lineNumber: index
+			}
+		};
 	});
 }
 
-function Statement( options ) {
-	if( options )
-		this.setOptions( options );
+function nestTokens( tokensLine ) {
+	var resultantNestedTokens = initResultantNestedTokens();
+
+	tokensLine.tokens.forEach( function step( token, index ) {
+		// body...
+	})
 }
 
-Statement.prototype.setOptions = function( options ) {
-	// type: 'output', 'variable', 'function'.
-	this.type = options.type;
-	// array of contents.
-	this.contents = options.contents;
-	// variable or function name.
-	this.reference = options.reference;
+function initResultantNestedTokens() {
+	var r = [], pn;
+
+	for( pn in resultantNestedTokensPrototype ) {
+		r[ pn ] = resultantNestedTokensPrototype[ pn ];
+	}
+
+	return r;
 }
+
+var resultantNestedTokensPrototype = {
+	addDeeper: function() { this.add( [] ).deeper(); return this; },
+	add: function( item ) { this.resolve().push( item ); return this; },
+	deeper: function() { this.path.push( this.resolve().length - 1 ); return this; },
+	shallower: function() { this.path.pop(); return this; },
+	top: function() { this.path.length = 0; return this; },
+	resolve: function() {
+		var goal = this;
+		var pi, pl;
+
+		for( pi = 0, pl = this.path.length; pi < pl; ++pi ) {
+			goal = goal[ this.path[ pi ] ];
+		}
+
+		return goal;
+	}
+};
--- src/caternator/tokenizer.js
+++ src/caternator/tokenizer.js
@@ -1,38 +1,16 @@
 // caternator/tokenizer - Breaks the input into tokens!
 
-// String -> Array<Array<String>>
-// Returns a 2D array: An array where each item represents a line,
-// and where each line contains an array of the tokens from that line.
-function tokenize( inputString ) {
-	var normalizedInput = normalize( inputString );
-	var lines = splitLines( normalizedInput );
-	return lines.map( tokenizeLine );
-}
+function tokenize( lineContainer ) {
+	lineContainer.tokens = splitTokensFromLine( normalizeLine( lineContainer.string ) );
 
-// normalize line-endings: trim and remove runs
-// space out certain characters: "(", ")", "="
-function normalize( inputString ) {
-	return inputString
-		.replace( /[\r\n]+/g, '\n' ) // remove runs, remove \r.
-		.replace( /^\n+|\n+$/g, '' ) // trim.
-		.replace( /[\(\)=]/g, ' $& ' ) // space out tokens.
-		;
-}
-
-function splitLines( normalizedInput ) {
-	return normalizedInput.split( '\n' );
-}
-
-function tokenizeLine( line ) {
-	var normedLine = normalizeLine( line );
-	return collectTokensFromLine( normedLine );
+	return lineContainer;
 }
 
 function normalizeLine( line ) {
 	return line.replace( /^\s+|\s+$/g, '' );
 }
 
-function collectTokensFromLine( line ) {
+function splitTokensFromLine( line ) {
 	var tokens = [];
 	var whitespaceSeeker = /\s+/g;
 	var match, lastIndex = 0, token;
@@ -58,4 +36,14 @@ function collectTokensFromLine( line ) {
 	return tokens;
 }
 
+function identifyTokens( lineContainer ) {
+	// something with lineContainer.tokens.
+	lineContainer.identifiedTokens = lineContainer.tokens.map( function identifySingleToken( token, index ) {
+		// ...
+	})
+}
+
 module.exports.tokenize = tokenize;
+module.exports.identifyTokens = identifyTokens;
+
+// Token Wrapper Objects, too, like Assingment Indicator, Group Begin Indicator, etc.
